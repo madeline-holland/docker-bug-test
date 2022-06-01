590b9865716e9200b3dc11c53b279676ff07e4b1
Madeline Holland <madeline@daggercat.com>
2022-06-01 17:59:23 -0500

    Update script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm out/before
-rm out/after
+echo "Cleaning up from prior runs"
+rm out/*
 
 docker compose up -d
 docker compose exec test /bin/bash -c 'ls -alh /tmp/ > /tmp/out/before'
@@ -8,13 +8,26 @@ docker compose exec test /bin/bash -c 'ls -alh /tmp/ > /tmp/out/before'
 echo "Before:"
 cat out/before
 
+echo "Making a change to test.md"
 echo aaa >> test.md
+docker compose exec test /bin/bash -c 'ls -alh /tmp/ > /tmp/out/change'
+cat out/change
 
+echo "Moving test.md"
+mv test.md /tmp/test.md
 
+echo "Moving test.md back"
+mv /tmp/test.md test.md
 
+docker compose exec test /bin/bash -c 'ls -alh /tmp/ > /tmp/out/movement'
+cat out/movement
+
+echo "Checking out to undo the change"
 git checkout -f -- test.md
 
-docker compose exec test /bin/bash -c 'ls -alh /tmp/ > /tmp/out/after'
+docker compose exec test /bin/bash -c 'ls -alh /tmp/ > /tmp/out/checkout'
 
-echo "After"
-cat out/after
+echo "After Git checkout"
+cat out/checkout
+
+docker compose down
